7fd63ffc81a353ff535de47b9bac82b746a95932
Jose Buitrago <55982719+j-buitrago@users.noreply.github.com>
2020-01-16 22:47:57 +0100

    Update createModel.R

--- R/createModel.R
+++ R/createModel.R
@@ -5,7 +5,7 @@ data$cyl <- as.factor(data$cyl)
 if (!require("randomForest")){
   install.packages("randomForest")
 }
-
+library(randomForest)
 rfModel <- randomForest(cyl ~ ., data = data)
 saveRDS(rfModel, "./rfModel.RDS")
            
